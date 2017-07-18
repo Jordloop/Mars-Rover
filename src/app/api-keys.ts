2b59a280e7451ae48df50bdfdaee79d30f947c8e
JordLoop <JordLoop@gmail.com>
2017-07-17 22:03:12 -0700

    add api

---
+++ src/app/api-keys.ts
@@ -0,0 +1,10 @@
+export const marsRoverKey = "QXGwHC8fQAg5umcout7gRl0nfk6MsyLDz0B2FZKJ";
+
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyArvbDRIP5IdarA1G_Advac3SY0a3xhZ0s",
+  authDomain: "rover-api-a5e2a.firebaseapp.com",
+  databaseURL: "https://rover-api-a5e2a.firebaseio.com",
+  projectId: "rover-api-a5e2a",
+  storageBucket: "rover-api-a5e2a.appspot.com",
+  messagingSenderId: "244283082522"
+};
--- src/app/rover-form/rover-form.component.ts
+++ src/app/rover-form/rover-form.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { MarsRoverAPIPhotos } from '../mars-rover-api-photos.service';
+import { PhotoService } from '../photo.service';
 
 @Component({
   selector: 'app-rover-form',
   templateUrl: './rover-form.component.html',
-  styleUrls: ['./rover-form.component.css']
+  styleUrls: [ './rover-form.component.css' ],
+  providers: [ MarsRoverAPIPhotos, PhotoService ]
 })
-export class RoverFormComponent implements OnInit {
 
-  constructor() { }
+export class RoverFormComponent {
+  photos: any[]=null;
+  noPhotos: boolean=false;
+  constructor(private marsRoverPhotos: MarsRoverAPIPhotos) { }
 
-  ngOnInit() {
+  getRoverImages(date: string, camera: string) {
+    this.noPhotos = false;
+    this.marsRoverPhotos.getByDateAndCamera(date, camera).subscribe(response =>{
+      if(response.json().photos.length > 0)
+      {
+        this.photos = response.json();
+      }
+      else {
+        this.noPhotos = true;
+      }
+    });
   }
 
+  saveRoverImages(date, camera){
+    this.marsRoverPhotos.saveImages(date, camera);
+    alert("The images from " + date + "taken by the " + camera + " camera have been saved to the database.")
+  }
 }
